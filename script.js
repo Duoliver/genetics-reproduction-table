8db72de7f0e1ffed4a48b58789a7e4502c1faa54
Honeysalad <oliveira.douglasedu@outlook.com>
2020-11-29 01:25:32 -0300

    setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const handleClick = () => {
+    const first = document.getElementById('first').value
+    const second = document.getElementById('second').value
+
+    console.log(first, second)
+}
