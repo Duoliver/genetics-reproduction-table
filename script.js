1a5fcf36195b812c59443defed023ebb7dcd14ed
Honeysalad <oliveira.douglasedu@outlook.com>
2020-11-29 03:09:54 -0300

    mvp

--- script.js
+++ script.js
@@ -6,34 +6,44 @@ const generateSequence = (genotype) => {
 };
 
 const orderAlleleByCase = (alA, alB) => {
-    return /^[A-Z]/.test(alA)
-    ? alA + alB
-    : alB + alA;
-}
+  return /^[A-Z]/.test(alA) ? alA + alB : alB + alA;
+};
 
 const generateGenotype = (pairA, pairB) => {
-  const firstGene = orderAlleleByCase(pairA[0], pairB[0])
-  const secondGene = orderAlleleByCase(pairA[1], pairB[1])
+  const firstGene = orderAlleleByCase(pairA[0], pairB[0]);
+  const secondGene = orderAlleleByCase(pairA[1], pairB[1]);
   return firstGene + secondGene;
 };
 
 const handleClick = () => {
+  document.getElementById("container").innerHTML = "";
   const first = document.getElementById("first").value;
   const second = document.getElementById("second").value;
   if (first.length !== second.length && first.length !== 4) {
     return false;
   }
-  const firstSequence = generateSequence(first);
-  const secondSequence = generateSequence(second);
-  const fullSequence = [...firstSequence, ...secondSequence];
-  const matrix = []
-  fullSequence.forEach(pairX => {
-      const row = []
-      fullSequence.forEach(pairY => {
-        row.push(generateGenotype(pairX, pairY))
-      })
-      matrix.push(row)
+  const fullSequence = [
+    ...generateSequence(first),
+    ...generateSequence(second),
+  ];
+  const matrix = [["", ...fullSequence]];
+  fullSequence.forEach((pairX) => {
+    const row = [pairX];
+    fullSequence.forEach((pairY) => {
+      row.push(generateGenotype(pairX, pairY));
+    });
+    matrix.push(row);
+  });
+  matrix.map(row => {
+    document.getElementById("container").innerHTML += `
+      <div class="row">
+        ${
+          row.map(genotype => `
+            <div>${genotype}</div>
+          `).join('')
+        }
+      </div>
+    `;
   })
-
-  console.log(matrix)
+  console.log(matrix);
 };
